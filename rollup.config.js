951137c8c3d4a3bff4b93e78acb30a21f42d14be
Kevin <moohng@126.com>
2021-03-26 11:39:56 +0800

    目录结构更改

--- rollup.config.js
+++ rollup.config.js
@@ -16,53 +16,14 @@ module.exports = {
   ],
   output: [
     {
-      file: 'dist/tui.common.js',
-      format: 'cjs',
-    },
-    {
-      file: 'dist/tui.common.min.js',
-      format: 'cjs',
-      plugins: [
-        terser(),
-      ],
-    },
-    {
-      file: 'dist/tui.es.js',
-      format: 'es',
-    },
-    {
-      file: 'dist/tui.es.min.js',
-      format: 'es',
-      plugins: [
-        terser(),
-      ],
-    },
-    {
       file: 'dist/tui.js',
-      format: 'iife',
-      name: 'tui',
-      // https://rollupjs.org/guide/en#output-globals-g-globals
-      globals: {},
-    },
-    {
-      file: 'dist/tui.min.js',
-      format: 'iife',
-      name: 'tui',
-      // https://rollupjs.org/guide/en#output-globals-g-globals
-      globals: {},
-      plugins: [
-        terser(),
-      ],
-    },
-    {
-      file: 'dist/tui.umd.js',
       format: 'umd',
       name: 'tui',
       // https://rollupjs.org/guide/en#output-globals-g-globals
       globals: {},
     },
     {
-      file: 'dist/tui.umd.min.js',
+      file: 'dist/tui.min.js',
       format: 'umd',
       name: 'tui',
       // https://rollupjs.org/guide/en#output-globals-g-globals
