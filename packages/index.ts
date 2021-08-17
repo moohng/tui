ecadb8a3991be83846fcd6e985511e12784a4f08
mohong <mohong@zmn.com>
2021-08-17 11:19:22 +0800

    导出核心api

--- packages/index.ts
+++ packages/index.ts
@@ -2,3 +2,4 @@ export { default as Toast } from './Toast/index';
 export { default as Dialog } from './Dialog/index';
 export { default as Preview } from './Preview/index';
 export { default as Swiper } from './core/swiper';
+export { transition, fadeIn, fadeOut, scaleIn, scaleOut } from './core/animate';
